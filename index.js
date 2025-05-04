a5c9e83dc671cabc26d1d65f8b6c57f3a3a63e79
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-05-05 01:42:26 +0600

    user created successfully

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
-const morgan = require("morgan");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nugjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) return res.status(401).send({ error: "Unauthorized" });
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(403).send({ error: "Forbidden" });
+
+    req.user = decoded;
+    next();
+  });
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nugjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,8 +46,51 @@ async function run() {
     await client.connect();
     console.log("Mongodb connected");
 
-    app.use(cors());
-    app.use(express.json());
+    const database = client.db("micro_tasks");
+    const usersCollection = database.collection("users");
+
+    app.post("/jwt", async (req, res) => {
+      const { email } = req.body;
+      if (!email) return res.status(400).send({ error: "Email is required" });
+
+      const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "Strict",
+        })
+        .send({ success: true });
+    });
+
+    app.get("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "Strict",
+        })
+        .send({ success: true });
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    // create all users here
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "already user is logging" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Sample route
     app.get("/", (req, res) => {
